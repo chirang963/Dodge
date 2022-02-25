8296d85ddaf4184e6318f7ffcdcc5204572e906e
chirang963 <qawsedrf963@naver.com>
2022-02-25 17:31:01 +0900

    02.25
    bullet or spawn

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -5,59 +5,75 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // 이동에 사용할 리지드바디 컴포넌트
-    public Rigidbody PlayerRigidbody;
-    // 이동속력
-    float speed = 8f;
-    // Start is called before the first frame update
+    public Rigidbody playerRigidbody;
+    // 이동 속력
+    public float speed = 3f;
 
+    // 내 자신을 담을 변수
     public GameObject my;
-    void Start()
-    {
-        //게임 오브젝트에서 Rigidbody 컴포넌트를 찾아
-        //PlayerRigidbody에 할당
-        PlayerRigidbody = GetComponent<Rigidbody>(); //Rigidbody가 비어있을때
 
+    private void Start()
+    {
+        // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아
+        // playerRigidbody에 할당
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //수평축과 수직축의 입력값을 감지해서 저장
-        float xinput = Input.GetAxis("Horizontal");
-        //키보드 : "A", ←: 음의방향 : -1.0f
-        //키보드 : "D", →: 양의방향 : +1.0f
-        float zinput = Input.GetAxis("vertical");
-        //키보드 : "W", ↑: 양의방향 : +1.0f
-        //키보드 : "S", ↓: 음의방향 : -1.0f
+        // 수평축과 수직축의 입력값을 감지해서 저장
+        float xInput = Input.GetAxis("Horizontal");
+        // 키보드 : 'A', <- : 음의방향 : -1.0f
+        // 키보드 : 'D', -> : 양의방향 : +1.0f
+        float zInput = Input.GetAxis("Vertical");
+        // 키보드 : 'W', ^ : 양의방향 : +1.0f
+        // 키보드 : 'S', ▼ : 음의방향 : -1.0f
+
+        // 실제 이동 속도를 입력값과
+        // 이동 속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        // Vector3 속도를 (xSpeed, 0f, zSpeed) 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+        // 리지디바디의 속도에 newVelocity 할당
+        playerRigidbody.velocity = newVelocity;
+
     }
 
-
-    void Directinput()
+    void DirectInput()
     {
         if (Input.GetKey(KeyCode.UpArrow) == true)
         {
-            PlayerRigidbody.AddForce(0f, 0f, speed);
+            // 위쪽 방향키 입력이 감지된 경우
+            playerRigidbody
+                .AddForce(0f, 0f, speed);
         }
-
         if (Input.GetKey(KeyCode.DownArrow) == true)
         {
-            PlayerRigidbody.AddForce(0f, 0f, -speed);
+            // 아래쪽 방향키 입력이 감지된 경우
+            // -z 방향 힘 주기
+            playerRigidbody
+                .AddForce(0f, 0f, -speed);
         }
-
         if (Input.GetKey(KeyCode.RightArrow) == true)
         {
-            PlayerRigidbody.AddForce(speed, 0f, 0f);
+            // 오른쪽 방향키 입력이 감지된 경우
+            // x 방향 힘 주기
+            playerRigidbody
+                .AddForce(speed, 0f, 0f);
         }
-
         if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
-            PlayerRigidbody.AddForce(-speed, 0f, 0f);
+            // 왼쪽 방향키 입력이 감지된 경우
+            // -x 방향 힘 주기
+            playerRigidbody
+                .AddForce(-speed, 0f, 0f);
         }
     }
 
-    void Die()
+    public void Die()
     {
         my.SetActive(false);
-        //gameObject.SetActive(false);
     }
 }
